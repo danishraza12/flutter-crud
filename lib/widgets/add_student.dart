112cf8c8ef320aa8501bd9b5ad2e0bfd2702bc99
Muhammad-Bilal-Rizwan97 <60816244+Muhammad-Bilal-Rizwan97@users.noreply.github.com>
2021-12-16 16:11:13 +0500

    Perform Validation On Add Student Page

--- lib/widgets/add_student.dart
+++ lib/widgets/add_student.dart
@@ -1,6 +1,7 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, deprecated_member_use
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import '../services/apis.dart';
 
 class AddStudent extends StatefulWidget {
@@ -14,6 +15,9 @@ class _AddStudentState extends State<AddStudent> {
   final NameController = TextEditingController();
   final AgeController = TextEditingController();
   final CityController = TextEditingController();
+  bool _validate1 = false;
+  bool _validate2 = false;
+  bool _validate3 = false;
 
   @override
   Widget build(BuildContext context) {
@@ -33,36 +37,48 @@ class _AddStudentState extends State<AddStudent> {
         Container(
           width: 300,
           alignment: Alignment.center,
-          margin: new EdgeInsets.symmetric(horizontal: 35, vertical: 12),
+          margin: EdgeInsets.symmetric(horizontal: 35, vertical: 12),
           child: TextField(
             controller: NameController,
+            inputFormatters: [
+              WhitelistingTextInputFormatter(RegExp("[a-zA-Z]")),
+            ],
             decoration: InputDecoration(
               border: OutlineInputBorder(),
               labelText: 'Name',
+              errorText: _validate1 ? 'Enter Correct Name Field' : null,
             ),
           ),
         ),
         Container(
           width: 300,
-          margin: new EdgeInsets.symmetric(horizontal: 35, vertical: 12),
+          margin: EdgeInsets.symmetric(horizontal: 35, vertical: 12),
           alignment: Alignment.center,
           child: TextField(
             controller: AgeController,
+            inputFormatters: [
+              WhitelistingTextInputFormatter(RegExp("[0-9]")),
+            ],
             decoration: InputDecoration(
               border: OutlineInputBorder(),
               labelText: 'Age',
+              errorText: _validate2 ? 'Enter Correct Age Field' : null,
             ),
           ),
         ),
         Container(
           width: 300,
-          margin: new EdgeInsets.symmetric(horizontal: 35, vertical: 12),
+          margin: EdgeInsets.symmetric(horizontal: 35, vertical: 12),
           alignment: Alignment.center,
           child: TextField(
             controller: CityController,
+            inputFormatters: [
+              WhitelistingTextInputFormatter(RegExp("[a-zA-Z]")),
+            ],
             decoration: InputDecoration(
               border: OutlineInputBorder(),
               labelText: 'City',
+              errorText: _validate3 ? 'Enter Correct City Field' : null,
             ),
           ),
         ),
@@ -73,29 +89,26 @@ class _AddStudentState extends State<AddStudent> {
             if (NameController.text.isEmpty ||
                 AgeController.text.isEmpty ||
                 CityController.text.isEmpty) {
-              showDialog(
-                context: context,
-                builder: (context) {
-                  return const AlertDialog(
-                    title: Text(
-                      'Warning!',
-                      textAlign: TextAlign.center,
-                    ),
-                    content: Text('Enter all values'),
-                    contentTextStyle: TextStyle(
-                      color: Colors.blue,
-                      fontSize: 15,
-                    ),
-                    titleTextStyle: TextStyle(
-                        color: Colors.orange,
-                        fontSize: 25,
-                        fontWeight: FontWeight.bold),
-                  );
-                },
-              );
+              setState(() {
+                NameController.text.isEmpty
+                    ? _validate1 = true
+                    : _validate1 = false;
+                AgeController.text.isEmpty
+                    ? _validate2 = true
+                    : _validate2 = false;
+                CityController.text.isEmpty
+                    ? _validate3 = true
+                    : _validate3 = false;
+              });
             } else {
               postStudents(context, NameController.text, AgeController.text,
                   CityController.text);
+
+              setState(() {
+                _validate1 = false;
+                _validate2 = false;
+                _validate3 = false;
+              });
 
               NameController.clear();
               AgeController.clear();
